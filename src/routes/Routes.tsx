a8786b61ae8f53a5a050488772fbd904c2ec6db0
isha-web1 <isha.web1@gmail.com>
2025-02-12 00:47:08 +0600

    refactor array reduce

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,39 +1,37 @@
-import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
-
-import {AdminPath} from "./admin.routes";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import { adminRoutes } from './admin.routes';
 
 const router = createBrowserRouter([
-    {
-        path : '/',
-        element : <App/>,
-        
-    },
-    {
-        path : '/admin',
-        element : <App/>,
-        children : AdminPath
-    },
-    {
-        path : '/faculty',
-        element : <App/>,
-        children : AdminPath
-    },
-    {
-        path : '/student',
-        element : <App/>,
-        children : AdminPath
-    },
-    {
-        path : '/login',
-        element : <Login/>
-    },
-    {
-        path : '/register',
-        element : <Register/>
-    }
-])
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/admin',
+    element: <App />,
+    children: adminRoutes,
+  },
+  {
+    path: '/faculty',
+    element: <App />,
+    children: adminRoutes,
+  },
+  {
+    path: '/student',
+    element: <App />,
+    children: adminRoutes,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+]);
 
 export default router;
