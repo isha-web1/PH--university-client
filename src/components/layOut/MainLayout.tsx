1b228a2703d157739886a8f029101c21650406de
isha-web1 <isha.web1@gmail.com>
2025-06-11 22:25:32 +0600

    create authSlice & authApi layer by inject in baseApi

--- src/components/layOut/MainLayout.tsx
+++ src/components/layOut/MainLayout.tsx
@@ -1,62 +1,35 @@
-import { Layout, Menu, MenuProps } from "antd";
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
-import { Children, createElement } from "react";
-import { NavLink, Outlet } from "react-router-dom";
-import { adminPaths,  } from "../../routes/admin.routes";
-import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
-import Sidebar from "./Sidebar";
+import { Layout, Menu, MenuProps } from 'antd';
+import {
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { createElement } from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+import { adminPaths, adminSidebarItems } from '../../routes/admin.routes';
+import { sidebarItemsGenerator } from '../../utils/sidebarItemsGenerator';
+import Sidebar from './Sidebar';
 const { Header, Content, Footer, Sider } = Layout;
 
-// const items : MenuProps['items'] = [
-//     {
-//         key : 'Dashboard',
-//         label : <NavLink to='/admin/dashboard'>Dashboard</NavLink>,
-//     },
-//     {
-//         key : '2',
-//         label : 'Profiles',
-//     },
-//     {
-//         key : 'User Management',
-//         label : 'User Management',
-//         children : [
-//             {
-//                 key : 'create-Admin',
-//                 label :  <NavLink to='/admin/create-admin'>Create-admin</NavLink>,
-//             },
-//             {
-//                 key : 'create-faculty',
-//                 label :  <NavLink to='/admin/create-faculty'>create-faculty</NavLink>,
-//             },
-//             {
-//                 key : 'create-student',
-//                 label :  <NavLink to='/admin/create-student'>create-student</NavLink>,
-//             },
-//         ]
-//     }
-// ];
-
 const MainLayout = () => {
-    return (
-        <Layout style={{height :'100vh'}}>
-        <Sidebar></Sidebar>
-        <Layout>
-          <Header style={{ padding: 0, }} />
-          <Content style={{ margin: '24px 16px 0' }}>
-            <div
-              style={{
-                padding: 24,
-                minHeight: 360,
-              
-              }}
-            >
-              <Outlet/>
-            </div>
-          </Content>
-         
-        </Layout>
+  return (
+    <Layout style={{ height: '100vh' }}>
+      <Sidebar />
+      <Layout>
+        <Header style={{ padding: 0 }} />
+        <Content style={{ margin: '24px 16px 0' }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            <Outlet />
+          </div>
+        </Content>
       </Layout>
-    );
+    </Layout>
+  );
 };
 
 export default MainLayout;
--- src/utils/sidebarItemsGenerator.tsx
+++ src/utils/sidebarItemsGenerator.tsx
@@ -1,33 +1,28 @@
+import { TSidebarItem, TUserPath } from '../types';
+import { NavLink } from 'react-router-dom';
 
-import { NavLink } from "react-router-dom";
-import { TSidebarItem, TUserPath } from "../types";
+export const sidebarItemsGenerator = (items: TUserPath[], role : string) => {
+  const sidebarItems = items.reduce((acc: TSidebarItem[], item) => {
+    if (item.path && item.name) {
+      acc.push({
+        key: item.name,
+        label: <NavLink to={`/${role}/${item.path}`}>{item.name}</NavLink>,
+      });
+    }
 
- 
+    if (item.children) {
+      acc.push({
+        key: item.name,
+        label: item.name,
+        children: item.children.map((child) => ({
+          key: child.name,
+          label: <NavLink to={`/${role}/${child.path}`}>{child.name}</NavLink>,
+        })),
+      });
+    }
 
-export const sidebarItemsGenerator = (items : TUserPath[], role) =>{
-    const SidebarItems = items.reduce(
-        (acc: TSidebarItem[], item) => {
-          if (item.path && item.name) {
-            acc.push({
-              key: item.name,
-              label: <NavLink to={`/${role}/${item.path}`}>{item.name}</NavLink>,
-            });
-          }
-      
-          if (item.children) {
-            acc.push({
-              key: item.name,
-              label: item.name,
-              children: item.children.map((child) => ({
-                key: child.name,
-                label: <NavLink to={`/${role}/${child.path}`}>{child.name}</NavLink>,
-              })),
-            });
-          }
-      
-          return acc;
-        },
-        []
-      );
-      return SidebarItems
-}
+    return acc;
+  }, []);
+
+  return sidebarItems;
+};
