42c05f07c3f23ce4f16d23bad9d5bb3003cede2f
isha-web1 <isha.web1@gmail.com>
2025-06-18 23:45:43 +0600

    courseManagement types,courseMangement api & createCouurseManagement added

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,4 @@ export * from './sidebarTypes';
 export * from './global';
 export * from './academicManagement.types';
 export * from './userManagement.type';
+export * from './courseManagement.types';
