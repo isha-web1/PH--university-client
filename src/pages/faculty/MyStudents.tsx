6ae1890fec9e8c4d73bb7008240e4705c6073336
isha-web1 <isha.web1@gmail.com>
2025-06-21 17:27:50 +0600

    faculty myCourses & myStudents components added

--- src/pages/faculty/MyStudents.tsx
+++ src/pages/faculty/MyStudents.tsx
@@ -1,11 +1,128 @@
-import React from 'react';
+import { useParams } from 'react-router-dom';
+import {
+  useAddMarkMutation,
+  useGetAllFacultyCoursesQuery,
+} from '../../redux/features/faculty/facultyCourses.api';
+import { Button, Modal, Table, TableColumnsType } from 'antd';
+import { useState } from 'react';
+import PHForm from '../../components/form/PHForm';
+import PHSelect from '../../components/form/PHSelect';
+import PHInput from '../../components/form/PHInput';
 
 const MyStudents = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const { registerSemesterId, courseId } = useParams();
+  const { data: facultyCoursesData } = useGetAllFacultyCoursesQuery([
+    { name: 'semesterRegistration', value: registerSemesterId },
+    { name: 'course', value: courseId },
+  ]);
+
+  console.log(facultyCoursesData);
+
+  type FacultyCourseDataItem = {
+    _id: string;
+    student: { _id: string; id: string; fullName: string };
+    semesterRegistration: { _id: string };
+    offeredCourse: { _id: string };
+  };
+
+  const tableData = facultyCoursesData?.data?.map(
+    ({ _id, student, semesterRegistration, offeredCourse }: FacultyCourseDataItem) => ({
+      key: _id,
+      name: student.fullName,
+      roll: student.id,
+      semesterRegistration: semesterRegistration._id,
+      student: student._id,
+      offeredCourse: offeredCourse._id,
+    })
+  );
+
+  const columns = [
+    {
+      title: 'Name',
+      key: 'name',
+      dataIndex: 'name',
+    },
+    {
+      title: 'Roll',
+      key: 'roll',
+      dataIndex: 'roll',
+    },
+    {
+      title: 'Action',
+      key: 'x',
+      render: (item : any) => {
+        return (
+          <div>
+            <AddMarksModal studentInfo={item} />
+          </div>
+        );
+      },
+    },
+  ];
+
+  return <Table columns={columns} dataSource={tableData} />;
+};
+
+type StudentInfo = {
+  key: string;
+  name: string;
+  roll: string;
+  semesterRegistration: string;
+  student: string;
+  offeredCourse: string;
+};
+
+const AddMarksModal = ({ studentInfo }: { studentInfo: StudentInfo }) => {
+  console.log(studentInfo);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [addMark] = useAddMarkMutation();
+
+  const handleSubmit = async (data : any) => {
+    const studentMark = {
+      semesterRegistration: studentInfo.semesterRegistration,
+      offeredCourse: studentInfo.offeredCourse,
+      student: studentInfo.student,
+      courseMarks: {
+        classTest1: Number(data.classTest1),
+        midTerm: Number(data.midTerm),
+        classTest2: Number(data.classTest2),
+        finalTerm: Number(data.finalTerm),
+      },
+    };
+
+    console.log(studentMark);
+    const res = await addMark(studentMark);
+
+    console.log(res);
+  };
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <Button onClick={showModal}>Add Faculty</Button>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        <PHForm onSubmit={handleSubmit}>
+          <PHInput type="text" name="classTest1" label="Class Test 1" />
+          <PHInput type="text" name="classTest2" label="Class Test 2" />
+          <PHInput type="text" name="midTerm" label="Midterm" />
+          <PHInput type="text" name="finalTerm" label="Final" />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Modal>
+    </>
+  );
 };
 
 export default MyStudents;
